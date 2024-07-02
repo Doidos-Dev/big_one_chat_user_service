9527717791ba91b4823e39970a6577a47ddc240f
jaofreire <joao.gabriel18872@gmail.com>
2024-07-01 23:26:49 -0300

    create models

---
+++ Domain/Entities/SettingsModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class SettingsModel
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public bool IsVisibleStatus { get; private set; }
+        public bool IsVisibleLastSeen { get; private set; }
+        public bool IsVisibleMessageSeen { get; private set; }
+
+        public SettingsModel(Guid id,
+            Guid userId,
+            bool isVisibleStatus,
+            bool isVisibleLastSeen,
+            bool isVisibleMessageSeen)
+        {
+            Id = id;
+            UserId = userId;
+            IsVisibleStatus = isVisibleStatus;
+            IsVisibleLastSeen = isVisibleLastSeen;
+            IsVisibleMessageSeen = isVisibleMessageSeen;
+        }
+
+        public SettingsModel()
+        {
+            
+        }
+    }
+}
---
+++ Domain/Entities/UserModel.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class UserModel
+    {
+        public Guid Id { get; private set; }
+        public string? Name { get; private set; }
+        public string? Nickname { get; private set; }
+        public StatusEnum Status { get; private set; }
+        public string? Photo {  get; private set; }
+        public string? Email { get; private set; }
+        public string? Password { get; private set; }
+        public DateTime? LastSeen { get; private set; }
+        public DateTime? CreatedDate { get; private set; }
+
+        public UserModel(Guid id,
+            string name,
+            string nickname,
+            StatusEnum status,
+            string photo,
+            string email,
+            string password,
+            DateTime? lastSeen,
+            DateTime? createdDate)
+        {
+            Id = id;
+            Name = name;
+            Nickname = nickname;
+            Status = status;
+            Photo = photo;
+            Email = email;
+            Password = password;
+            LastSeen = lastSeen;
+            CreatedDate = createdDate;
+        }
+
+        public UserModel()
+        {
+            
+        }
+    }
+}
