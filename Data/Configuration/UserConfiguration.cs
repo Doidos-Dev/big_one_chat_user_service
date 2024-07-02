be5b2d242e212061c8c078b184d3012d4e5dcdf8
Isaias Vasconcelos <isaiaskeyboards@gmail.com>
2024-07-02 14:25:21 -0300

    Adicionado as configurações e injeções da classe de contexto de leitura e escrita

---
+++ Data/Configuration/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<UserModel>
+    {
+        public void Configure(EntityTypeBuilder<UserModel> builder)
+        {
+            builder.ToTable("users");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasColumnName("NAME").HasMaxLength(32).IsRequired();
+            builder.Property(x => x.Nickname).HasColumnName("NICK_NAME").HasMaxLength(32).IsRequired();
+            builder.Property(x => x.Status).HasColumnName("STATUS").IsRequired();
+            builder.Property(x => x.Photo).HasColumnName("PHOTO").HasMaxLength(255).IsRequired();
+            builder.Property(x => x.Email).HasColumnName("EMAIL").HasMaxLength(128).IsRequired();
+            builder.Property(x => x.Password).HasColumnName("PASSWORD").HasMaxLength(128).IsRequired();
+            builder.Property(x => x.LastSeen).HasColumnName("LAST_SEEN");
+            builder.Property(x => x.CreatedDate).HasColumnName("CREATED_DATE");
+        }
+    }
+}
