65d790863d63ba60d36826143bf729943a90e928
jaofreire <joao.gabriel18872@gmail.com>
2024-07-03 11:04:55 -0300

    suporte ao docker, criação de variaveis de ambiente

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,17 @@
+using API;
 using DependencyInjection.Ext;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddConfig();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var environment = APIConfiguration.LocalEnvironment;
+
+builder.Services.AddConfig(environment == APIConfiguration.LocalEnvironment
+    ? builder.Configuration["Postgres:ConnectionStrings"]
+    : Environment.GetEnvironmentVariable("DATABASE_CONNECTION_SERVICE_USER"));
 
 var app = builder.Build();
 
